e48a86441fc7e2f65be3cc3ba1de40612f8237b4
ManuelAcMn <115465144+ManuelAcMn@users.noreply.github.com>
2023-05-22 13:27:04 +0200

    Update 1.py
    Made

--- 1.py
+++ 1.py
@@ -15,6 +15,10 @@ grid_sizef = 5
 x_Enemy = random.randint(1,grid_sizef-1)
 y_Enemy = random.randint(1,grid_sizef-1)
 blankSpace = '#'
+if x_Enemy == x and y_Enemy == y:
+    x_Enemy = random.randint(1,grid_sizef-1)
+    y_Enemy = random.randint(1,grid_sizef-1)
+
 def display_game(game, grid_size):
     for i in range(grid_size):
         for j in range(grid_size):
@@ -25,30 +29,13 @@ def display_game(game, grid_size):
             else:
                 print(f"{game} ", end='')
         print("")
-def NextMove():
-    newpos = []
-    newpos.append(x- x_Enemy)
-    newpos.append(y - y_Enemy)
-    if newpos[0] > 1:
-        newpos[0] = 1
-    elif newpos[0] < -1:
-        newpos[0] = -1
-    if newpos[1] > 1:
-        newpos[0] = 1
-    elif newpos[1] < -1:
-        newpos[1] = -1
-    x_Enemy += newpos[0]
-    y_Enemy += newpos[1]
-    return newpos
-
 running = True
 clear()
-print("En este juego, eres un & y tienes que huir de *")
+print("En este juego, eres un & y tienes que huir de *, puedes coger . para vencer a *")
 Debug = input("Quieres entrar en modo DEBUG, veras cosas--> ")
 blankSpace = input("Dime que quieres que sea el espacio en blanco, si lo dejas e blanco sera #--> ")
 if blankSpace =="" or blankSpace== " ":
     blankSpace = '#'
-arraysa = []
 while running:
     clear()
     display_game(blankSpace, grid_sizef)
@@ -57,21 +44,30 @@ while running:
         print("Posicion Y jugador: " +str(y))
         print("Posicion X enemigo: " +str(x_Enemy))
         print("Posicion Y enemigo: " +str(y_Enemy))
-        print(arraysa)
     opc = input("Arriba(w), abajo(s), izquierda(a), derecha(d), sal(q): ")
-    arraysa = NextMove() #-1 IZQ 1 Der, 
-    if opc == "q":
+    x_EnemyN = x - int(x_Enemy)
+    y_EnemyN = y - int(y_Enemy)
+    if x_EnemyN > 0:
+        x_EnemyN = 1
+    elif x_EnemyN < 0:
+        x_EnemyN = -1
+    if y_EnemyN > 0:
+        y_EnemyN = 1
+    elif y_EnemyN < 0:
+        y_EnemyN = -1
+    x_Enemy += x_EnemyN
+    y_Enemy += y_EnemyN
+    if opc == "q" or opc == "Q":
         running = False
-    if opc == "w":
+    if opc == "w" or opc == "W":
         x -= 1
-    if opc == "s":
+    if opc == "s" or opc == "S":
         x += 1
-    if opc == "a":
+    if opc == "a" or opc == "A":
         y -= 1
-    if opc == "d":
+    if opc == "d" or opc == "D":
         y += 1
     if x == x_Enemy and y == y_Enemy:
         clear()
         running = False
         print("Has muerto")
-        #sys.exit()
